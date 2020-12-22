9daeeee2f077edbdade98d4f3c02f918d2542b83
warlock001 <75542810+warlock001@users.noreply.github.com>
2020-12-23 00:41:39 +0500

    bazinga

--- src/DoublyLinkedListDS/Main.java
+++ src/DoublyLinkedListDS/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("wo hoooo");
         int id;
         boolean exit=false;
         DoublyLinkedList doublyLinkedList = DoublyLinkedList.getInstance();
